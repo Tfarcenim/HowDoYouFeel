b210874c273ae93a2fdde25c3c4a8d2ab8bf53c8
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-08-16 22:21:12 -0400

    boilerplate

--- fabric/src/main/java/tfar/howdoyoufeel/platform/FabricPlatformHelper.java
+++ fabric/src/main/java/tfar/howdoyoufeel/platform/FabricPlatformHelper.java
@@ -1,6 +1,6 @@
-package com.example.examplemod.platform;
+package tfar.howdoyoufeel.platform;
 
-import com.example.examplemod.platform.services.IPlatformHelper;
+import tfar.howdoyoufeel.platform.services.IPlatformHelper;
 import net.fabricmc.loader.api.FabricLoader;
 
 public class FabricPlatformHelper implements IPlatformHelper {
--- forge/src/main/java/tfar/howdoyoufeel/platform/ForgePlatformHelper.java
+++ forge/src/main/java/tfar/howdoyoufeel/platform/ForgePlatformHelper.java
@@ -1,6 +1,6 @@
-package com.example.examplemod.platform;
+package tfar.howdoyoufeel.platform;
 
-import com.example.examplemod.platform.services.IPlatformHelper;
+import tfar.howdoyoufeel.platform.services.IPlatformHelper;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLLoader;
 
